8065923461e723df4413b50f0c31e32ce8bab69a
Vladislav Melnichuk <melnichuk1712@mail.ru>
2023-03-06 23:35:53 +0300

    Added movement
    blablalba

--- SFML Project/Source.cpp
+++ SFML Project/Source.cpp
@@ -7,7 +7,11 @@ int main()
 {
     RenderWindow window(VideoMode(1280, 720), "Game!");
     RectangleShape rect;
-    rect.setPosition(Vector2f(1280, 720));
+    rect.setSize(Vector2f(150, 150));
+    rect.setFillColor(Color::Blue);
+    float pX, pY;
+    pX = pY = 400;
+    rect.setPosition(Vector2f(pX, pY));
 
     while (window.isOpen())
     {
@@ -17,13 +21,30 @@ int main()
             if (event.type == Event::Closed)
                 window.close();
 
-            if (Keyboard::isKeyPressed(Keyboard::Escape)) {
-                window.close();
+            if (Keyboard::isKeyPressed(Keyboard::D)) {
+                pX += 5;
+                Vector2f newPos = Vector2f(pX, pY);
+                rect.setPosition(newPos);
+            }
+            if (Keyboard::isKeyPressed(Keyboard::A)) {
+                pX -= 5;
+                Vector2f newPos = Vector2f(pX, pY);
+                rect.setPosition(newPos);
+            }
+            if (Keyboard::isKeyPressed(Keyboard::W)) {
+                pY -= 5;
+                Vector2f newPos = Vector2f(pX, pY);
+                rect.setPosition(newPos);
+            }
+            if (Keyboard::isKeyPressed(Keyboard::S)) {
+                pY += 5;
+                Vector2f newPos = Vector2f(pX, pY);
+                rect.setPosition(newPos);
             }
         }
 
         window.clear();
-        window.draw(shape);
+        window.draw(rect);
         window.display();
     }
 
